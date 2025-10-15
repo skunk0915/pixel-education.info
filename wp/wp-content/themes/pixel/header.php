d354317328f4b1fff8a8b7aa5097db1e35637570
mizow <mizoguchi@outward.jp>
2025-10-15 18:06:18 +0900

    アコーディオン開閉アイコン

--- wp/wp-content/themes/pixel/header.php
+++ wp/wp-content/themes/pixel/header.php
@@ -17,6 +17,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@500&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=keyboard_arrow_up" />
 	<style>
 		/* ページ読み込み中は白画面を表示 */
 		body::before {
