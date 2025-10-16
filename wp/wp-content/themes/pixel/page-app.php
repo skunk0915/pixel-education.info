2127e7edd3ab701a7e28922260e6b77e8fac7bc7
mizow <mizoguchi@outward.jp>
2025-10-16 12:36:03 +0900

    アプリ一覧ページで動的に記事取得

--- wp/wp-content/themes/pixel/page-app.php
+++ wp/wp-content/themes/pixel/page-app.php
@@ -7,7 +7,39 @@
 	</section><!-- /.mv -->
 	<main>
 		<div class="body_bg">
-			
+			<div class="app_list_wrap">
+				<?php
+				$args = array(
+					'post_type' => 'app',
+					'posts_per_page' => -1,
+					'orderby' => 'date',
+					'order' => 'DESC'
+				);
+				$app_query = new WP_Query($args);
+
+				if ($app_query->have_posts()) :
+					while ($app_query->have_posts()) : $app_query->the_post();
+				?>
+					<article class="app_list_item">
+						<?php if (has_post_thumbnail()) : ?>
+							<div class="app_thumbnail">
+								<?php the_post_thumbnail('medium'); ?>
+							</div>
+						<?php endif; ?>
+						<h2 class="app_name"><?php the_title(); ?></h2>
+						<div class="app_excerpt">
+							<?php the_excerpt(); ?>
+						</div>
+						<a href="<?php the_permalink(); ?>" class="app_link">詳細を見る</a>
+					</article>
+				<?php
+					endwhile;
+					wp_reset_postdata();
+				else :
+				?>
+					<p>現在、登録されているアプリはありません。</p>
+				<?php endif; ?>
+			</div><!-- /.app_list_wrap -->
 		</div><!-- /.body_bg -->
 	</main>
 </body>
