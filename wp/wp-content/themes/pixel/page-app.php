bf27370e35f97f6fe867e7d9f36bb8acb2d51998
mizow <mizoguchi@outward.jp>
2025-10-29 11:29:40 +0900

    アプリ一覧：カスタムフィールド値取得

--- wp/wp-content/themes/pixel/page-app.php
+++ wp/wp-content/themes/pixel/page-app.php
@@ -29,27 +29,81 @@
 						$icon_url = $has_icon ? esc_url($app_icon) : esc_url(get_theme_file_uri('/img/logo_mark_pixel.png'));
 				?>
 						<li class="app_list_item">
+							<a href="">
+								<div class="block_a">
+									<div class="app_thumbnail">
+										<img src="<?php echo $icon_url; ?>" alt="<?php the_title_attribute(); ?>" class="<?php echo !$has_icon ? 'default-icon' : ''; ?>">
+									</div>
+									<div class="status">
+										<?php
+										// 価格の表示
+										$price = get_field('price');
+										if ($price !== '' && $price !== null && $price !== false) :
+										?>
+											<div class="price">
+												<?php echo ($price == 0) ? '無料' : '&yen;' . esc_html($price); ?>
+											</div>
+										<?php endif; ?>
 
-							<div class="block_a">
-								<div class="app_thumbnail">
-									<img src="<?php echo $icon_url; ?>" alt="<?php the_title_attribute(); ?>" class="<?php echo !$has_icon ? 'default-icon' : ''; ?>">
+										<?php
+										// 対象年齢の表示
+										$age_min = get_field('age_min');
+										$age_max = get_field('age_max');
+										$has_min = !empty($age_min);
+										$has_max = !empty($age_max);
+										?>
+										<div class="age">
+											<?php
+											if (!$has_min && !$has_max) {
+												// 両方値がない場合
+												echo '全年齢';
+											} elseif ($has_min && $has_max) {
+												// 両方値がある場合
+												if ($age_min == $age_max) {
+													echo esc_html($age_min) . '歳';
+												} else {
+													echo esc_html($age_min) . '歳～' . esc_html($age_max) . '歳';
+												}
+											} elseif ($has_min) {
+												// age_minのみ値がある場合
+												echo esc_html($age_min) . '歳以上';
+											} else {
+												// age_maxのみ値がある場合
+												echo esc_html($age_max) . '歳以下';
+											}
+											?>
+										</div>
+									</div>
 								</div>
-								<div class="status">
-									<div class="price">無料</div>
-									<div class="age">全年齢</div>
-								</div>
-							</div>
-							<div class="app_txt">
+								<div class="app_txt">
 
-								<h2 class="app_name"><?php the_title(); ?></h2>
-								<div class="app_excerpt">
-									<?php echo get_excerpt_with_linebreaks(get_the_ID()); ?>
-								</div>
-							</div><!-- /.app_txt -->
+									<h2 class="app_name"><?php the_title(); ?></h2>
+									<div class="app_excerpt">
+										<?php echo get_excerpt_with_linebreaks(get_the_ID()); ?>
+									</div>
+								</div><!-- /.app_txt -->
+							</a>
+
 							<div class="store">
-								<a href=""><img src="<?php echo get_theme_file_uri(); ?>/img/btn_ios.png" alt="iPhone版アプリをダウンロードする"></a>
-								<a href=""><img src="<?php echo get_theme_file_uri(); ?>/img/btn_android.png" alt="Android版アプリをダウンロードする"></a>
-							</div>
+								<?php
+								$ios_url = get_field('ios_url');
+								if (!empty($ios_url)) :
+								?>
+									<a href="<?php echo esc_url($ios_url); ?>" target="_blank">
+										<img src="<?php echo get_theme_file_uri(); ?>/img/btn_ios.png" alt="iPhone版アプリをダウンロードする">
+									</a>
+								<?php endif; ?>
+								<?php
+								$android_url = get_field('android_url');
+								if (!empty($android_url)) :
+								?>
+									<a href="<?php echo esc_url($android_url); ?>" target="_blank">
+										<img src="<?php echo get_theme_file_uri(); ?>/img/btn_android.png" alt="Android版アプリをダウンロードする">
+									</a>
+								<?php endif; ?>
+							</div><!-- /.store -->
+
+
 
 						</li>
 					<?php
